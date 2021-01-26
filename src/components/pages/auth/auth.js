e5ad8cb1a2c5880ecc29a7ef4c5edbbd8a6c4e55
baza04 <bazarbay.amir@gmail.com>
2021-01-26 16:48:35 +0600

    feat: add dev and prod links

--- src/components/pages/auth/auth.js
+++ src/components/pages/auth/auth.js
@@ -1,9 +1,13 @@
 import React from "react";
-// import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 
-const authUrl =
+const devUrl =
   "https://git.01.alem.school/login/oauth/authorize/?client_id=d02ae1b0-9b05-4b24-ad87-1ef2a9ea2559&redirect_uri=https://dashboard.alem.school/api/auth/git&response_type=code&state=http://localhost:3000/auth";
+
+const prodUrl =
+  "https://git.01.alem.school/login/oauth/authorize/?client_id=d02ae1b0-9b05-4b24-ad87-1ef2a9ea2559&redirect_uri=https://dashboard.alem.school/api/auth/git&response_type=code&state=https://dashboard.alem.school/auth";
+
+const authUrl = process.env.NODE_ENV === "development" ? devUrl : prodUrl;
 
 const styles = {
   cont: {
