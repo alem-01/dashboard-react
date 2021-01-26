956a74d07af4a0d2f8f329dd539dec28a2e59396
baza04 <bazarbay.amir@gmail.com>
2021-01-26 20:30:17 +0600

    feat: add dark style

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Container from "@material-ui/core/Container";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 import Header from "../header";
 import GetToken from "../get-token";
@@ -15,55 +17,67 @@ export default class App extends Component {
     loading: false,
   };
 
+  theme = createMuiTheme({
+    palette: {
+      type: "dark",
+      background: {
+        paper: "#1F1F1F",
+      },
+    },
+  });
+
   render() {
     return (
-      <ErrorBoundry>
-        <Container
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            marginTop: "7vh",
-          }}
-        >
-          <Router>
-            <Header />
-            <Switch>
-              <Route path="/" exact component={Auth} />
-              <Route path="/auth" exact component={GetToken} />
+      <ThemeProvider theme={this.theme}>
+        <CssBaseline />
+        <ErrorBoundry>
+          <Container
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: "5%",
+            }}
+          >
+            <Router>
+              <Header />
+              <Switch>
+                <Route path="/" exact component={Auth} />
+                <Route path="/auth" exact component={GetToken} />
 
-              <Route path="/leaderboard" component={Leaderboard} />
-              <Route
-                path="/profile/:id"
-                render={({ match }) => {
-                  const { id } = match.params;
-                  return <Profile login={id} />;
-                }}
-              />
+                <Route path="/leaderboard" component={Leaderboard} />
+                <Route
+                  path="/profile/:id"
+                  render={({ match }) => {
+                    const { id } = match.params;
+                    return <Profile login={id} />;
+                  }}
+                />
 
-              {/* 
-              <Route
-                path="/profile/:id/piscine"
-                exact
-                render={({ match }) => {
-                  // const { id } = match.params;
-                  return (
-                    <>
-                      // want do universal profile header with basic info
-                      <StudentInfo login={id} /> 
-                      
-                      // then render piscine info
-                      <PiscineInfo />
-                    </>
-                  );
-                }}
-              /> */}
+                {/*
+        <Route
+        path="/profile/:id/piscine"
+        exact
+        render={({ match }) => {
+        // const { id } = match.params;
+        return (
+        <>
+        // want do universal profile header with basic info
+        <StudentInfo login={id} />
+        
+        // then render piscine info
+        <PiscineInfo />
+        </>
+        );
+        }}
+        /> */}
 
-              <Route render={() => <ErrorIndicator type="404" />} />
-            </Switch>
-          </Router>
-        </Container>
-      </ErrorBoundry>
+                <Route render={() => <ErrorIndicator type="404" />} />
+              </Switch>
+            </Router>
+          </Container>
+        </ErrorBoundry>
+      </ThemeProvider>
     );
   }
 }
