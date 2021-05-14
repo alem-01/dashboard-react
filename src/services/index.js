26c1833fa280afb7c7ecde002de3fddc57bc83ec
baza04 <bazarbay.amir@gmail.com>
2021-05-15 03:41:26 +0600

    refactor: unit service funcs

--- src/services/index.js
+++ src/services/index.js
@@ -42,7 +42,45 @@ function getMaxXp(xpArr, needConvert = false) {
   return Math.max(...xpArr);
 }
 
-export { convertXp, getMaxXp, humanFileSize };
+function descendingComparator(a, b, orderBy) {
+  a = orderBy === "login" ? a[orderBy].toLowerCase() : a[orderBy];
+  b = orderBy === "login" ? b[orderBy].toLowerCase() : b[orderBy];
+  if (b < a) {
+    return -1;
+  }
+  if (b > a) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+function AddOrderNum(arr) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr[i] = {
+      ...arr[i],
+      num: i + 1,
+    };
+  }
+  return newArr;
+}
+export { AddOrderNum, convertXp, getComparator, getMaxXp, humanFileSize, stableSort };
 
 // const url = "https://dashboard.alem.school/api/leaderboard";
 // const authUrl =
