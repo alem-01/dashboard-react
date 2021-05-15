615c73227dd3dfb96fb03be6d775a9a959109a40
baza04 <bazarbay.amir@gmail.com>
2021-05-15 17:27:27 +0600

    refactor: divide getPiscineInfo

--- src/services/dashboard-service.js
+++ src/services/dashboard-service.js
@@ -1,68 +1,21 @@
 export default class DashboardServices {
   _apiUrl = "https://dashboard.alem.school/api";
 
-  getData = async (url = "") => {
+  getData = async (url = "", redirect = true) => {
     const response = await fetch(`${this._apiUrl}${url}`, {
       headers: {
         Authorization: localStorage.getItem("token"),
       },
-    }).catch((err) => {
-      console.log(url, err);
     });
-    if (response.ok === false) {
+
+    if (!response.ok) {
       console.log("failed res: ", response);
-      window.location.replace("/");
+      if (redirect) window.location.replace("/");
       throw new Error(
         `Couldn't fetch: ${this._apiUrl}${url} status: ${response.status}`
       );
     }
-    const body = await response.json();
-
-    return body;
-  };
-
-  getAllStudents = async () => {
-    const res = await this.getData("/leaderboard");
-    return res.standings.map(this._transformStudent);
-  };
-
-  getStudentAvatar = async (login) => {
-    return await this.fetchStudentInfo(login, "/avatar");
-  };
-
-  getProgressInfo = async (login) => {
-    const [
-      basicInfo, // id, login, first, last name, phone
-      aggregate, // xp, audits and all projects[date, name, grade] status(pass, failed, in process)
-      audit_ratio, // audits: up, down
-      progress, // total_xp by date
-      piscine_quests, // all tasks: parent: Quest#, child: task_name, grade
-      piscine_exams, // all tasks: parent: exam#, child: task_name, grade(1||0)
-      piscine_raids, // all raids: parent(sudoku), attrs: baza04, serzhan, etc.., grade
-    ] = await Promise.all([
-      this.fetchStudentInfo(login),
-      this.fetchStudentInfo(login, "/aggregate"), // all started project, with status(pass, failed, in process)
-      this.fetchStudentInfo(login, "/audit_ratio"),
-      this.fetchStudentInfo(login, "/progress_bar"),
-      this.fetchStudentInfo(login, "/piscine/quests"),
-      this.fetchStudentInfo(login, "/piscine/exams"),
-      this.fetchStudentInfo(login, "/piscine/raids"),
-    ]);
-    // need add "/attendance", "/attendance/aggregate" fetching
-
-    return {
-      basicInfo,
-      aggregate,
-      audit_ratio,
-      progress,
-      piscine_quests,
-      piscine_exams,
-      piscine_raids,
-    };
-  };
-
-  fetchStudentInfo = async (login, dataType = "") => {
-    return await this.getData(`/user/${login}${dataType}`);
+    return await response.json();
   };
 
   _transformStudent(student) {
@@ -74,6 +27,50 @@ export default class DashboardServices {
       generation: student.generation,
     };
   }
+
+  getAllStudents = async () => {
+    const res = await this.getData("/leaderboard");
+    return res.standings.map(this._transformStudent);
+  };
+
+  getStudentAvatar = async (login) => {
+    return await this.getData(`/user/${login}/avatar`, false);
+  };
+
+  getProgressInfo = async (login) => {
+    const [
+      basicInfo, // id, login, first, last name, phone
+      aggregate, // xp, audits and all projects[date, name, grade] status(pass, failed, in process)
+      audit_ratio, // audits: up, down
+      progress, // total_xp by date
+    ] = await Promise.all([
+      this.getData(`/user/${login}`),
+      this.getData(`/user/${login}/aggregate`),
+      this.getData(`/user/${login}/audit_ratio`),
+      this.getData(`/user/${login}/progress_bar`),
+    ]);
+    // need add "/attendance", "/attendance/aggregate" fetching
+
+    return {
+      basicInfo,
+      aggregate,
+      audit_ratio,
+      progress,
+    };
+  };
+
+  getPiscineInfo = async (login) => {
+    const [
+      piscine_quests, // all tasks: parent: Quest#, child: task_name, grade
+      piscine_exams, // all tasks: parent: exam#, child: task_name, grade(1||0)
+      piscine_raids, // all raids: parent(sudoku), attrs: baza04, serzhan, etc.., grade
+    ] = await Promise.all([
+      this.getData(`/user/${login}/piscine/quests`),
+      this.getData(`/user/${login}/piscine/exams`),
+      this.getData(`/user/${login}/piscine/raids`),
+    ]);
+    return { piscine_quests, piscine_exams, piscine_raids };
+  };
 
   /* _extractLogin(item) {
     console.log("extract : ", item);
